542cbbae391b5e389ad8c8e4eb5cee15077f9ee7
Akib358 <akibkhondokar81@gmail.com>
2026-07-12 21:49:07 +0600

    fix: change 3rd

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CoPC-2026/',
+  base: './',
   server: {
     port: 5173,
   },
